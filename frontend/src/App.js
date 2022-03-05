f98dfca189b9a36fbcc3ec40466708b2eac47ab4
stikx <stikx2432@gmail.com>
2022-03-04 19:35:17 -0800

    product-list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,33 @@
+import data from './data';
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">amazona</a>
       </header>
-      <main>List products</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.images} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong> ${product.price} </strong>
+                </p>
+                <p>{product.description}</p>
+                <button> Add to Cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
